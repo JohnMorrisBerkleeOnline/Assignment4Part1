fd97c5ef5d7821aa9aab88b237902ca00328f1bc
JohnMorrisBerkleeOnline <41553512+JohnMorrisBerkleeOnline@users.noreply.github.com>
2018-07-22 20:52:02 -0500

    Add files via upload

---
+++ assignment 4 part 1.js
@@ -0,0 +1,11 @@
+functon myset_list(place_in_race);{
+  let place_in_race = [
+    {Richard: place_in_race.indexOf(0},
+    {Gina: place_in_race.indexOf(1)},
+    {Tobias: place_in_race.indexOf(2)}
+];
+  place_in_race.shift("Jimbo");
+  place_in_race.pop("Tobias");
+}
+
+console.log(place_in_race);
